fc9e0702596d48cc2c39dc8e56f0097f476fc096
Anay-Shrivastava <anayshrivastava1999@gmail.com>
2025-11-25 17:32:12 +0530

    Adding New Features like Add product

--- Admin-Panel/src/Components/Sidebaar.jsx
+++ Admin-Panel/src/Components/Sidebaar.jsx
@@ -6,16 +6,16 @@ import { NavLink } from 'react-router-dom'
 const Sidebaar = () => {
   return (
     <div className='w-[18%] h-[100vh] border border-gray-400 solid  border-top-0 font-[1vw, 10px] '>
-      <div className='pt-[50px] pl-[20%] flex flex-col gap-[30px] '>
-        <NavLink to='/add' className='flex items-center gap-[12px] border border-[1px] solid border-[#a9a9a9] border-right-0 p-[8px 10px] rounded-[3px 0px 3px] cursor-pointer'>
+      <div className=' pt-[50px] pl-[20%] flex flex-col gap-[30px] '>
+        <NavLink to='/add' className='sidebaar-option flex items-center gap-[12px] border border-[1px] solid border-[#a9a9a9] border-right-0 p-[8px 10px] rounded-[3px 0px 3px] cursor-pointer'>
           <img src={add} alt="" />
           <p>Add Items</p>
         </NavLink>
-        <NavLink to='/list' className='flex items-center gap-[12px] border border-[1px] solid border-[#a9a9a9] border-right-0 p-[8px 10px] rounded-[3px 0px 3px] cursor-pointer'>
+        <NavLink to='/list' className='sidebaar-option flex items-center gap-[12px] border border-[1px] solid border-[#a9a9a9] border-right-0 p-[8px 10px] rounded-[3px 0px 3px] ml-[-4] cursor-pointer'>
           <img src={order} alt="" />
           <p>List Items</p>
         </NavLink>
-        <NavLink to='/order' className='flex items-center gap-[12px] border border-[1px] solid border-[#a9a9a9] border-right-0 p-[8px 10px] rounded-[3px 0px 3px] cursor-pointer'>
+        <NavLink to='/order' className='sidebaar-option flex items-center gap-[12px] border border-[1px] solid border-[#a9a9a9] border-right-0 p-[8px 10px] rounded-[3px 0px 3px] cursor-pointer'>
           <img src={order} alt="" />
           <p>Orders</p>
         </NavLink>
--- Admin-Panel/src/Pages/Add.jsx
+++ Admin-Panel/src/Pages/Add.jsx
@@ -1,9 +1,47 @@
 import React from 'react'
+import image from '../assets/upload.webp'
 
 const Add = () => {
   return (
-    <div>
-      Add
+    <div className=''>
+      <form >
+        <div>
+          <p>Upload Image</p>
+          <label htmlFor="image">
+            <img src={image} alt=""  className='w-20 h-20'/>
+          </label>
+          <input type="file" hidden required  />
+        </div>
+        <div>
+          <p>Product Name</p>
+          <input type="text"name='name' placeholder='Type name' />
+        </div>
+        <div>
+          <p>Product Decsription</p>
+          <textarea name="description" rows='6' placeholder='Write content here' required></textarea>
+        </div>
+        <div>
+          <div>
+            <p>Product Category</p>
+            <select name="category">
+              <option value="Salad">Salad</option>
+              <option value="Rolls">Rolls</option>
+              <option value="Desert">Desert</option>
+              <option value="Sandwich">Sandwich</option>
+              <option value="Cake">Cake</option>
+              <option value="Pure-Veg">Pure-Veg</option>
+              <option value="Pasta">Pasta</option>
+              <option value="Noodles">Noodles</option>
+            </select>
+          </div>
+          <div>
+            <p>Product Price</p>
+            <input type="number" name='price' placeholder='$20' />
+          </div>
+        </div>
+        <button type='submit'>ADD</button>
+      </form>
+      
     </div>
   )
 }
