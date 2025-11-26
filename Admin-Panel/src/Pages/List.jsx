37005d85b2aa005d15b904d22146e166f87c47cd
Anay-Shrivastava <anayshrivastava1999@gmail.com>
2025-11-26 17:10:49 +0530

    Adding features Food-Add And foood -delete from list

--- Admin-Panel/src/Pages/List.jsx
+++ Admin-Panel/src/Pages/List.jsx
@@ -10,7 +10,7 @@ const List = () => {
 
   const fetchList = async () =>{
     const response = await axios.get(`${url}/api/food/list`);
-    console.log(response);
+    
     
     if(response.data.success) {
       setList(response.data.data)
@@ -22,6 +22,21 @@ const List = () => {
      
   }
 
+  const removeItem = async (foodId) =>{
+    
+    const response = await axios.post(`${url}/api/food/remove/`,{id:foodId})
+     await fetchList();
+     if(response.data.success){
+      toast.success("Item removed successfully")
+     }
+     else{
+      toast.error("Failed to remove the Item")
+     }
+    
+
+    
+  }
+
   useEffect(() =>{
   fetchList()
   },[])
@@ -30,25 +45,27 @@ const List = () => {
 
 
   return (
-    <div>
-      <p>All Foods List</p>
+    <div className='w-full h-100vh p-4 bg-gray-300 flex flex-col gap-4'>
+      <p className='items-center mx-auto flex justify-center text-2xl font-bold mt-10 '>All Foods List</p>
       <div>
-        <div className='flex'>
+        <div className='flex justify-between  font-semibold border-b-2  pb-2 mb-2 text-xl mt-[50px]'>
           <b>Name</b>
           <b>Price</b>
           <b>Category</b>
           <b>description</b>
           <b>Image</b>
+          <b>Delete</b>
         </div>
         {list.map((item, index) =>{
           return(
             <div >
-              <div key={index} className='flex gap-4'>
+              <div key={index} className='flex justify-between border-b-1 p-2 items-center text-lg mx-auto'>
                 <span>{item.name}</span>
                 <span>{item.price}</span>
                 <span>{item.category}</span>
                 <span>{item.description}</span>
-                <img src={item.image} alt={item.name}  className='w-20 h-20'/>
+                <img src={`${url}/images/`+item.image} alt={item.name}  className='w-20 h-20'/>
+                <p onClick={() =>removeItem(item._id)} className='cursor-pointer'>❌</p>
               </div>
             </div>
           )
